8113196a7ecb29f2d7cf34288e811b141efa9f61
EloToJaa <elotoja@protonmail.com>
2025-01-18 18:48:29 +0100

    Create schema

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,3 +1,4 @@
+import { sql } from "drizzle-orm";
 import * as t from "drizzle-orm/sqlite-core";
 import { sqliteTable as table } from "drizzle-orm/sqlite-core";
 
@@ -5,12 +6,45 @@ export const todos = table(
   "todos",
   {
     id: t.integer().primaryKey({ autoIncrement: true }),
-    name: t.text(),
-    description: t.text(),
-    completed: t.integer({ mode: "boolean" }),
-    order: t.integer(),
-    createdAt: t.integer({ mode: "timestamp" }),
-    updatedAt: t.integer({ mode: "timestamp" }),
+    listId: t
+      .integer()
+      .notNull()
+      .references(() => lists.id),
+    name: t.text().notNull().default(""),
+    description: t.text().notNull().default(""),
+    order: t.integer().notNull(),
+    completed: t.integer({ mode: "boolean" }).notNull().default(false),
+    createdAt: t
+      .integer({ mode: "timestamp" })
+      .notNull()
+      .default(sql`(unixepoch())`),
+    updatedAt: t
+      .integer({ mode: "timestamp" })
+      .notNull()
+      .default(sql`(unixepoch())`),
+  },
+  (table) => {
+    return {
+      orderIndex: t.uniqueIndex("order_idx").on(table.order),
+    };
+  },
+);
+
+export const lists = table(
+  "lists",
+  {
+    id: t.integer().primaryKey({ autoIncrement: true }),
+    name: t.text().notNull().default(""),
+    description: t.text().notNull().default(""),
+    order: t.integer().notNull(),
+    createdAt: t
+      .integer({ mode: "timestamp" })
+      .notNull()
+      .default(sql`(unixepoch())`),
+    updatedAt: t
+      .integer({ mode: "timestamp" })
+      .notNull()
+      .default(sql`(unixepoch())`),
   },
   (table) => {
     return {
